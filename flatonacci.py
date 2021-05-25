e46d9766b84c25708d3aefc59742c9db51f262cb
luis ases <mcmario9009@gmail.com>
2021-05-25 17:51:26 -0600

    Ejercisio de Secuencia de Flatonacci

---
+++ flatonacci.py
@@ -0,0 +1,36 @@
+from functools import reduce
+
+def flatonacci(signature: list, n: int) -> list:
+    # happy coding
+  resultado = []
+ 
+  try: 
+    if type(n) != int:
+      print('El dato ingresado '+str(n)+ 'es diferente a un numero entero')
+
+    elif n < 0:
+      print('El numero '+str(n)+' es negativo solo se premiten numeros enteros')
+  
+    else:
+      for valores in signature:
+        if type(valores) != int:
+          print('El listado contiene un str o un numero que no es entero')
+          return(resultado)
+        else:
+          resultado.append(valores)
+          print(resultado)
+
+      for i in range(n-3):
+        resultado.append(reduce(lambda x,y: x+y, resultado[:-4:-1]))
+
+      print(resultado)
+      return resultado
+    
+  except ValueError as err:
+    print(err)
+    return[]
+    
+flatonacci([0,2,1], 10)
+  
+  
+    
